73912b460b41dac597452ec5623e0729edb5f33a
saki-nagai <saki.nagai.734@baycurrent.co.jp>
2026-09-10 11:55:22 +0900

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: false,
+    formats: ['image/avif', 'image/webp'],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
